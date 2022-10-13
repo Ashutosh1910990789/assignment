174eae4ab6b7c8e607fb558a04e7e8e8a6db2fb5
Ashutosh1910990789 <80569566+Ashutosh1910990789@users.noreply.github.com>
2022-10-13 19:19:38 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,26 @@
+/*var is used to take variables 
+as int is taken in c java or c++
+ 1 
+
+  1 2 
+
+  1 2 3 
+
+  1 2 3 4 
+
+  1 2 3 4 5 
+
+  1 2 3 4 5 6 
+
+  1 2 3 4 5 6 7 
+
+  1 2 3 4 5 6 7 8
+  */
+ console.log("1")
+ console.log("1 2")
+ console.log("1 2 3")
+ console.log("1 2 3 4")
+ console.log("1 2 3 4 5")
+ console.log("1 2 3 4 5 6")
+ console.log("1 2 3 4 5 6 7")
+ console.log("1 2 3 4 5 6 7 8")
